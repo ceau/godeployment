3c641a8c88c977fd633cdf8e2a34e30d72924c9e
confused <496412785@qq.com>
2016-08-22 18:03:37 +0800

    make it better

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"github.com/russross/blackfriday"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":"+port, nil)
+
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
